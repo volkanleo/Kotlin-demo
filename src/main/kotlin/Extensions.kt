5090a00ad6d1a3fb5d08e51c76f1d51b2f6e66bc
volkanleo <volkan.ozturk@leovegas.com>
2023-10-11 16:11:52 +0200

    Added extensions

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -3,6 +3,23 @@ fun main() {
     println(name.slim())
 
     println(name.betterLength)
+
+    println("Test".getCustomName())
+    println(3.getCustomName())
+    println(3.5F.getCustomName())
+
+    println("----------")
+
+    val list = arrayListOf(3, 5, 6, 7, 3, 2, 7, 8)
+
+    println(list.howMany())
+    println("----------")
+
+    Book.printMe()
+
+    String.getClassType()
+
+    Double.printClassName()
 }
 
 // Extension function
@@ -15,3 +32,27 @@ val String.betterLength: Int
         println("Getting the better length")
         return 200
     }
+
+fun String.getCustomName() = "A string of characters"
+
+fun Int.getCustomName() = "A integer number"
+
+fun Float.getCustomName() = "A floating point number"
+
+fun ArrayList<Int>.howMany() = "This list contains $size elements"
+
+class Book {
+    companion object {}
+}
+
+fun Book.Companion.printMe() {
+    println("Book has been printed")
+}
+
+fun String.Companion.getClassType() {
+    println("This is a String class")
+}
+
+fun Double.Companion.printClassName() {
+    println("this is a Dobule class")
+}
